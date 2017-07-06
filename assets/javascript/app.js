8bc07807c837cc83cb939aaeb263ebeae1810b9a
kmboyle <kevin.michael.boyle@gmail.com>
2017-07-05 20:49:33 -0400

    gif animate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,29 +10,35 @@ function displayGiphy(){
 	var API = "b65d5f82fb254e96b900afe9bfd98bc1";
 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +gif+ "&api_key=" + API+ "&limit=10";
 
-	$("#gif-pics").empty();
-	
+	$(".gif-pics").empty();
+
 	$.ajax({
 		url: queryURL,
 		method: "GET"}).done(function(response){
 		for(var i = 0; i < 10; i++){
-			var gifDiv=$("<div class='gif'>");
+
+			var play =  response.data[i].images.fixed_height.url;
+
+			var image = response.data[i].images.fixed_height_still.url;
+
+
+			var gifDiv=$("<div>");
 
 			var rating = response.data[i].rating;
 
 			var pOne=$("<p>").text("Rating: " + rating);
-			console.log(response.data[i].rating);
+			
 
 			gifDiv.append(pOne);
 
 			
-			var image = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
+			var image = $("<img class='gif' data-still="+image+" data-state='still' data-animate="+play+">").attr("src", response.data[i].images.fixed_height_still.url);
+
 
 			gifDiv.append(image);
 
-			$("#gif-pics").prepend(gifDiv);
-
-			console.log(response.data[i].images.fixed_height_still.url);
+			
+			$(".gif-pics").prepend(gifDiv);
 		}
 
 		})
@@ -62,8 +68,27 @@ $("#add-gif").on("click", function(event) {
 	topic.push(react);
 
 	createButtons();
+
 });
 
 $(document).on("click",".reaction", displayGiphy);
 
 createButtons();
+
+$(document).on("click",".gif", function(){
+
+
+	var state = $(this).attr("data-state");
+	console.log(state);
+	console.log(this);
+
+	if(state === "still") {
+		$(this).attr("src", $(this).attr("data-animate"));
+		$(this).attr("data-state", "animate");
+	}
+	else {
+		$(this).attr("src", $(this).attr("data-still"));
+		$(this).attr("data-state","still");
+	}
+
+});
